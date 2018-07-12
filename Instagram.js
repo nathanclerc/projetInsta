b2366da1890525fc2bfbe8b591ba32628dc1d45c
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-07-12 10:00:42 +0200

    modif sur l'enregistrement et l'affichage des commentaires

--- Instagram.js
+++ Instagram.js
@@ -7,55 +7,63 @@ var like=0;
 var count_c=0;
 var coment=[];
 
-///////////////////////////////////
-	 $("#like").click(function()
-	 {   
-          like ++;
-        $("#count").text(like);
 
-	 });
+var coment = JSON.parse(window.localStorage.getItem("comment"));
+for (var i = 0; i < coment.length; i++) {
+		$('#commentaires').append('<p>' + coment[i] + '</p>');
+	}
 //////////////////////////////:
- 
 
-	$("#comment").click(function()
-		 {   
-				 	 $("#t_coment").show();
-			         $("#comment").hide(); 
-			         $("#ok").show();
-					 $("#ok").click( function()
-					 {  
-			
-	                    coment.push(localStorage.setItem("comment",$("#t_coment").val()));
+
+$("#comment").click(function()
+{   
+	$("#t_coment").show();
+	$("#comment").hide(); 
+	$("#ok").show();
+	
+
+}); 
+$("#ok").click( function(){
+
+	var commentaire = $("#t_coment").val();
+	var coment = JSON.parse(window.localStorage.getItem("comment"));
+	if (coment == null) {
+		var coment = [];
+		alert('Premier commentaire');
+	}
+	coment.push(commentaire);
+	var val = JSON.stringify(coment);
+	window.localStorage.setItem("comment", val);
 	                    //console.log(commentaire);
-						$("#t_coment").hide();
-						$("#ok").hide();
- 					    $("#comment").show(); 
-		      	       console.log(localStorage.getItem("comment"));
+	                    $("#t_coment").hide();
+	                    $("#ok").hide();
+	                    $("#comment").show();
+	$('#commentaires').empty();
+	for (var i = 0; i < coment.length; i++) {
+		$('#commentaires').append('<p>' + coment[i] + '</p>');
+	}
+});
 
-					 });
-
-			
-		 }); 
 ////////////////////////////////////////////////////////////////////
 
-	 $("#like_0_1").click(function()
-	 {   
-            
- 			if (clickCount == 1)
-			 {
+$("#like_0_1").click(function()
+{   
 
-				clickCount = 0;
+	if (clickCount == 1)
+	{
 
-				likeCount--;
- 
-			}else
-			{
-				clickCount++;
+		clickCount = 0;
 
-				likeCount++;
-			}      
-			  $("#count_0_1").text(likeCount);
+		likeCount--;
+
+	}else
+	{
+		clickCount++;
+
+		likeCount++;
+	}      
+	$("#count_0_1").text(likeCount);
 
 
-	 })
+})
 //////////////////////////////////////////////////////////////////////
